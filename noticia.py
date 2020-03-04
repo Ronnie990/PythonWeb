aa58f6021365815ee876f78f5cc757cf5fd3674d
Ronnie990 <roh990@gmail.com>
2020-03-03 21:22:56 -0400

    adecuacion

--- noticia.py
+++
@@ -1,5 +0,0 @@
-class noticias:
-  def __init__(self):
-    self.titulo = "Noticias Ronnie"
-    self.subtitulo = "Paciente rumano da negativo a prueba de coronavirus"
-    self.contenido = "El Ministerio de Salud Publica informó que salió negativa la prueba de coronavirus que se le realizó al único de los tres rumanos que tenía la sintomatología  de la enfermedad."
